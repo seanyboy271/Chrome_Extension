abcee0993cf56ed26be063cc0a3446df9d8e68d5
Sean <kotrolass@vcu.edu>
2019-09-05 16:36:42 -0400

    created persistent storage

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
 var list = []
+
+//addURL(list)
+
 var start = new Date();
 // Do things here
 var finish = new Date();
@@ -6,19 +9,28 @@ var difference = new Date();
 difference.setTime(finish.getTime() - start.getTime());
 
 
+function addURL(urlList) {
+    chrome.tabs.query({ 'active': true, 'currentWindow': true },
+        function (tabs) {
+            urlList.push(tabs[0].url)
+            var testJSON = {"url": tabs[0].url, "time": difference}
+            chrome.storage.sync.set({"url": tabs[0].url, "time": difference}, function(){
+                document.getElementById("status").innerHTML  = "we did it"
+            })
+        }
+    );
 
 
-document.getElementById("button").onclick = function(){
-    addURL(list);
-    document.getElementById("hello").innerText= list
+}
+
+document.getElementById("button").onclick = function () {
     var div = document.getElementById('hello');
-    div.innerHTML = div.innerHTML + "<br> Difference: " + difference.getMilliseconds()
+    addURL(list);
+
+    chrome.storage.sync.get(["url"], function(result) { //This does work 
+        div.innerHTML = result.url
+      });
+
+    //list.toString() + "<br> Difference: " + difference.getMilliseconds()
 }
 
-function addURL(urlList){
-    chrome.tabs.query({'active': true, 'currentWindow': true},
-   function(tabs){
-      alert(tabs[0].url);
-   }
-);
-}
