75733fc36161a5e3e52962d241a3d0704226f42d
Sean Kotrola <kotrolass@vcu.edu>
2019-09-05 10:39:53 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -16,5 +16,9 @@ document.getElementById("button").onclick = function(){
 }
 
 function addURL(urlList){
-    urlList.push(window.location.toString())
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+      alert(tabs[0].url);
+   }
+);
 }
