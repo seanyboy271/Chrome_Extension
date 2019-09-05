b930d706830ca9e7c28da952cd994baa156b6887
Sean Kotrola <kotrolass@vcu.edu>
2019-09-05 18:32:07 -0400

    clean up

--- popup.js
+++ popup.js
@@ -1,7 +1,4 @@
 var list = []
-
-//addURL(list)
-
 var start = new Date();
 // Do things here
 var finish = new Date();
@@ -9,28 +6,19 @@ var difference = new Date();
 difference.setTime(finish.getTime() - start.getTime());
 
 
+
+
+document.getElementById("button").onclick = function () {
+    addURL(list);
+    document.getElementById("hello").innerText = list
+    var div = document.getElementById('hello');
+    div.innerHTML = div.innerHTML + "<br> Difference: " + difference.getMilliseconds()
+}
+
 function addURL(urlList) {
     chrome.tabs.query({ 'active': true, 'currentWindow': true },
         function (tabs) {
-            urlList.push(tabs[0].url)
-            var testJSON = {"url": tabs[0].url, "time": difference}
-            chrome.storage.sync.set({"url": tabs[0].url, "time": difference}, function(){
-                document.getElementById("status").innerHTML  = "we did it"
-            })
+            alert(tabs[0].url);
         }
     );
-
-
 }
-
-document.getElementById("button").onclick = function () {
-    var div = document.getElementById('hello');
-    addURL(list);
-
-    chrome.storage.sync.get(["url"], function(result) { //This does work 
-        div.innerHTML = result.url
-      });
-
-    //list.toString() + "<br> Difference: " + difference.getMilliseconds()
-}
-
