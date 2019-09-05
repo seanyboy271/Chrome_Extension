7c18d56ee52b2b6844239a535b3ce0977f1bec25
Sean Kotrola <kotrolass@vcu.edu>
2019-09-05 10:36:09 -0400

    added list to store urls

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+var list = []
 var start = new Date();
 // Do things here
 var finish = new Date();
@@ -8,7 +9,12 @@ difference.setTime(finish.getTime() - start.getTime());
 
 
 document.getElementById("button").onclick = function(){
-    document.getElementById("hello").innerText= window.location.href
+    addURL(list);
+    document.getElementById("hello").innerText= list
     var div = document.getElementById('hello');
     div.innerHTML = div.innerHTML + "<br> Difference: " + difference.getMilliseconds()
+}
+
+function addURL(urlList){
+    urlList.push(window.location.toString())
 }
