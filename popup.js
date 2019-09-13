cd02b6c80480c1db946de9db0b0ffc57041cb782
Sean Kotrola <kotrolass@vcu.edu>
2019-09-13 07:54:34 -0400

    UI Update

--- popup.js
+++ popup.js
@@ -15,63 +15,64 @@ chrome.runtime.sendMessage({ greeting: "plsCalculateThanks" }, function (respons
 
     chrome.storage.sync.get('urlList', function (result) {
         var hello = document.getElementById("hello");
-        
+
         //Build the table to show in the popup
-        if(response.status != "not happening chief"){
+        if (response.status != "not happening chief") {
             var list = response.status
             var table = document.createElement("table")
-            table.className = "table table-striped table-dark"
+            table.className = "table"
             var tableHead = document.createElement("thead")
             var headRow = document.createElement("tr")
             var websiteHeader = document.createElement("th")
             var timeHeader = document.createElement("th")
 
-        
+
             var textnode = document.createTextNode("Website")
             websiteHeader.appendChild(textnode)
             headRow.appendChild(websiteHeader)
-            textnode = document.createTextNode("Time")
+            textnode = document.createTextNode("Time (hours:minutes:seconds:ms)")
             timeHeader.appendChild(textnode)
             headRow.appendChild(timeHeader)
             tableHead.appendChild(headRow)
             table.appendChild(tableHead)
-            
-
-            
-                for (var i = 0; i < list.length; i++) {
-
-
-                    var time = new Date(list[i].time)
-                    var hours = time.getUTCHours();
-                    var minutes = time.getUTCMinutes();
-                    var seconds = time.getUTCSeconds();
-                    var milliseconds = time.getUTCMilliseconds();
-                    var timeString = hours + " Hours   " + minutes + " Minutes    " + seconds + " Seconds     " + milliseconds + " Milliseconds     ";
 
 
 
-                    var row = document.createElement("tr")
-                    var cell = document.createElement("td")
-                    textNode = document.createTextNode(list[i].url)
-                    cell.appendChild(textNode)
-                    row.appendChild(cell)
+            for (var i = 0; i < list.length; i++) {
 
-                    cell = document.createElement("td")
-                    textNode = document.createTextNode(timeString)
-                    cell.appendChild(textNode)
-                    row.appendChild(cell)
-                    table.appendChild(row)
 
-                }
+                var time = new Date(list[i].time)
+                var day = time.getUTCDay()
+                var hours = time.getUTCHours();
+                var minutes = time.getUTCMinutes();
+                var seconds = time.getUTCSeconds();
+                var milliseconds = time.getUTCMilliseconds();
+                var timeString = hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
 
-                hello.appendChild(table)
 
-                table.cellSpacing = "10px"
-                table.cellPadding = "10px"
-            
+
+                var row = document.createElement("tr")
+                var cell = document.createElement("td")
+                textNode = document.createTextNode(list[i].url)
+                cell.appendChild(textNode)
+                row.appendChild(cell)
+
+                cell = document.createElement("td")
+                textNode = document.createTextNode(timeString)
+                cell.appendChild(textNode)
+                row.appendChild(cell)
+                table.appendChild(row)
+
             }
-            else {
-                hello.innerHTML = "No Data to show right now"
+
+            hello.appendChild(table)
+
+            table.cellSpacing = "10px"
+            table.cellPadding = "10px"
+
+        }
+        else {
+            hello.innerHTML = "No Data to show right now"
         }
     })
 });
